215a97f104e94d5e17412fa84919c619ff411d05
ochicf <oscarchicfarre@gmail.com>
2018-10-23 20:57:34 +0200

    recreate pages for each language

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,26 @@
+/* eslint-disable prettier/prettier */
 /**
  * Implement Gatsby's Node APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const i18n = require('./src/i18n/config');
 
-// You can delete this file if you're not using it
+exports.onCreatePage = ({ page, actions }) => {
+  const { createPage, deletePage } = actions;
+  deletePage(page);
+
+  i18n.locales.map(locale => {
+    const localizedPath =
+      locale.id === i18n.defaultLocale ? page.path : locale.path + page.path;
+
+    return createPage(
+      Object.assign({}, page, {
+        path: localizedPath,
+        context: {
+          locale,
+        },
+      })
+    );
+  });
+};
