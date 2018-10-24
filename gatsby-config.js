307de4651ce817dd62e920718f5ddaa28d8e9a82
ochicf <oscarchicfarre@gmail.com>
2018-10-24 11:58:07 +0200

    change logo and page info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'MTK Space',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'MTK Space',
+        short_name: 'MTK',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/MTK-logo.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
